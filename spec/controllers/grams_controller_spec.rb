6d99c4016a5be2168213b33fccabbe26cdf5cb62
codingwollf <anu.garcha12@gmail.com>
2019-07-16 23:16:34 +0000

    Lesson 10 6 tests

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -9,6 +9,25 @@ RSpec.describe GramsController, type: :controller do
   end
 
 
+describe "grams#new action" do
+  it "should require users to be logged in" do
+    get :new
+    expect(response).to redirect_to new_user_session_path
+  end
+
+  it "should successfully show the new form" do
+    user = User.create(
+    email:                 'fakeuser@gmail.com',
+    password:              'secretPassword',
+    password_confirmation: 'secretPassword'
+    )
+    sign_in user
+
+    get :new
+    expect(response).to have_http_status(:success)
+  end
+end
+
   describe "grams#create action" do
 
   it "should require users to be logged in" do
