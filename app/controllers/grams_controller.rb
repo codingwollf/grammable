53e61b1d785d3b750e37019d46946cc49c1249af
codingwollf <anu.garcha12@gmail.com>
2019-07-26 21:01:17 +0000

    Carrierwave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,13 +1,20 @@
 class GramsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+	def destroy
+    	@gram = Gram.find_by_id(params[:id])
+    	return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
+    	@gram.destroy
+    	redirect_to root_path
+    end
 
 	def update
   		@gram = Gram.find_by_id(params[:id])
  		return render_not_found if @gram.blank?
-
+    return render_not_found(:forbidden) if @gram.user != current_user
  		 @gram.update_attributes(gram_params)
-
-  		if @gram.valid?
+      if @gram.valid?
     		redirect_to root_path
   		else
     		return render :edit, status: :unprocessable_entity
@@ -19,6 +26,7 @@ class GramsController < ApplicationController
 	end
 
 	def index
+    @grams = Gram.all
 	end
 
 	def show
@@ -29,6 +37,7 @@ class GramsController < ApplicationController
   	def edit
     	@gram = Gram.find_by_id(params[:id])
     	return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
  	end
 
 	def create
@@ -43,10 +52,10 @@ class GramsController < ApplicationController
 	  private
 
   	def gram_params
-    	params.require(:gram).permit(:message)
+    	params.require(:gram).permit(:message, :picture)
     end
 
-    def render_not_found
-    	render plain: 'Not Found :(', status: :not_found
-  	end
+    def render_not_found(status=:not_found)
+      render plain: "#{status.to_s.titleize} :(", status: status
+    end
 end
